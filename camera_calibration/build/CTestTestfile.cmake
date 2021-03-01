d6962a5467d5093458e531abe17dcac4b8e97c56
anishabahl <anisha.bahl@kcl.ac.uk>
2021-03-01 14:46:39 +0000

    removes build folder

--- camera_calibration/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ab20/fri_ros_ws/src/HSI_KUKA/camera_calibration
-# Build directory: /home/ab20/fri_ros_ws/src/HSI_KUKA/camera_calibration/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
